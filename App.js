bba00f24c39d58da2a94004c586d8375314f130b
ndrslmpk <andreas.lempik@gmail.com>
2021-10-27 08:30:17 +0200

    added simple Button

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import logo from "./assets/woly.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={logo} style={styles.logo} />
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>And another one!</Text>
+      <Text style={styles.instructions}>And another one!</Text>
       <StatusBar style='auto' />
+
+      <TouchableOpacity
+        onPress={() => alert("Hello World")}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,5 +27,22 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  logo: {
+    width: 360,
+    height: 240,
+  },
+  instructions: {
+    color: "#888",
+    fontSize: 24,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 15,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#fff",
   },
 });
